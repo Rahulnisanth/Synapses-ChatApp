d9f16a8da4f8d8af316911892a7c31ddcb1cd542
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-09-29 21:39:55 +0530

    fix the chat-header name fonts

--- client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -3,16 +3,19 @@ import { RiCloseFill } from "react-icons/ri";
 
 const ChatHeader = () => {
   return (
-    <div className="h-[10vh] border-b-2 border-[#2f303b] flex items-center justify-between px-20">
-      <div className="flex flex-row gap-5 items-center justify-between">
-        <p className="text-3xl tracking-widest text-neutral-400 font-semibold">
+    <div className="h-[10vh] md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full border-b-2 border-[#2f303b] flex items-center justify-between px-8">
+      {/* Channel/Message Name */}
+      <div>
+        <p className="text-2xl md:text-3xl font-light tracking-wider text-neutral-300">
           Name of the channel or message
         </p>
-        <div className="flex items-center justify-center">
-          <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all">
-            <RiCloseFill className="text-3xl" />
-          </button>
-        </div>
+      </div>
+
+      {/* Close Button */}
+      <div className="flex items-center">
+        <button className="text-neutral-500 hover:text-white focus:outline-none duration-300 transition-all">
+          <RiCloseFill className="text-3xl" />
+        </button>
       </div>
     </div>
   );
