8b90194e6557d866e580e6cb959bf5d93bb220a2
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-12-14 13:49:21 +0530

    added changes in index.js-server

--- server/index.js
+++ server/index.js
@@ -13,19 +13,27 @@ import channel_route from "./routes/channel_routes.js";
 dotenv.config();
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://synapses-chat-app.vercel.app"
+  ); // Replace with your frontend's URL
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Specify allowed methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Specify allowed headers
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // If you need to send cookies
+  next();
+});
 
 const database_url = process.env.DATABASE_URL;
 const port = process.env.PORT || 5000;
 
-// Additional CORS configuration
+// Middleware
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "https://synapses-chat-app.vercel.app",
     credentials: true,
   })
 );
-
-// Middleware
 app.use(cookieParser());
 app.use(express.json());
 
