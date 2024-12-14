2cf562d9083f18c7f56fb3378f5cfe557828d788
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-12-14 12:40:54 +0530

    added allow origin access in client side

--- client/src/lib/api-client.js
+++ client/src/lib/api-client.js
@@ -2,6 +2,10 @@ import { HOST } from "@/utils/constants";
 import axios from "axios";
 
 export const api_client = axios.create({
-  baseURL: HOST || "https://synapses-chat-app-server.vercel.app",
+  baseURL: "https://synapses-chat-app-server.vercel.app",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
 });
